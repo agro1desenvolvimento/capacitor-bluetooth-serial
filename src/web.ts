239b6240dacc09709e4dff37fd8704bbf9f84058
Gerson Groth <ge.groth@gmail.com>
2019-11-05 14:59:34 -0300

    Add web methods

--- src/web.ts
+++ src/web.ts
@@ -9,10 +9,42 @@ export class BluetoothSerialWeb extends WebPlugin implements BluetoothSerialPlug
     });
   }
 
-  async echo(options: { value: string }): Promise<{value: string}> {
-    console.log('ECHO', options);
-    return options;
+  async isEnabled(): Promise<import("./definitions").BluetoothEnabledResult> {
+    throw new Error("Method not implemented.");
   }
+
+  async scan(): Promise<import("./definitions").BluetoothScanResult> {
+    throw new Error("Method not implemented.");
+  }
+
+  async connect(options: import("./definitions").BluetoothConnectOptions): Promise<void> {
+    throw new Error("Method not implemented.");
+  }
+
+  async disconnect(options: import("./definitions").BluetoothConnectOptions): Promise<void> {
+    throw new Error("Method not implemented.");
+  }
+
+  async isConnected(options: import("./definitions").BluetoothConnectOptions): Promise<import("./definitions").BluetoothConnectResult> {
+    throw new Error("Method not implemented.");
+  }
+
+  async read(options: import("./definitions").BluetoothReadOptions): Promise<import("./definitions").BluetoothDataResult> {
+    throw new Error("Method not implemented.");
+  }
+
+  async readUntil(options: import("./definitions").BluetoothReadUntilOptions): Promise<import("./definitions").BluetoothDataResult> {
+    throw new Error("Method not implemented.");
+  }
+
+  async enableNotifications(options: import("./definitions").BluetoothEnableNotificationsOptions): Promise<import("./definitions").BluetoothEnableNotificationsResult> {
+    throw new Error("Method not implemented.");
+  }
+
+  async disableNotifications(options: import("./definitions").BluetoothDisableNotificationsOptions): Promise<void> {
+    throw new Error("Method not implemented.");
+  }
+
 }
 
 const BluetoothSerial = new BluetoothSerialWeb();
